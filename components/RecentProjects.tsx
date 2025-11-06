49ab3192acea35c5704b5b6ee25e0d4e637c1b6f
rihabtaboubi1 <taboubirihab64@gmail.com>
2025-11-06 11:29:56 +0100

    refactor: simplify project links in RecentProjects
    - Move link props directly to PinContainer
    - Remove unnecessary wrapper element
    - Improve code organization

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,64 +18,62 @@ const RecentProjects = () => {
             key={item.id}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            {/* Envelopper PinContainer avec un lien */}
-            <a
+            {/* Passer le lien directement dans PinContainer */}
+            <PinContainer
+              title={item.title}
               href={item.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="w-full h-full"
             >
-              <PinContainer title={item.title}>
-                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                  <div
-                    className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                    style={{ backgroundColor: "#13162D" }}
-                  >
-                    <img
-                      src="/bg.png"
-                      alt="bgimg"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
                   <img
-                    src={item.img}
-                    alt="cover"
-                    className="z-10 absolute bottom-0"
+                    src="/bg.png"
+                    alt="bgimg"
+                    className="w-full h-full object-cover"
                   />
                 </div>
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0"
+                />
+              </div>
 
-                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                  {item.title}
-                </h1>
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
 
-                <p
-                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                  style={{ color: "#BEC1DD", margin: "1vh 0" }}
-                >
-                  {item.des}
-                </p>
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{ color: "#BEC1DD", margin: "1vh 0" }}
+              >
+                {item.des}
+              </p>
 
-                <div className="flex items-center justify-between mt-7 mb-3">
-                  <div className="flex items-center">
-                    {item.iconLists.map((icon, index) => (
-                      <div
-                        key={index}
-                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                        style={{
-                          transform: `translateX(-${5 * index + 2}px)`,
-                        }}
-                      >
-                        <img src={icon} alt="icon" className="p-2" />
-                      </div>
-                    ))}
-                  </div>
-
-                  <div className="flex justify-center items-center">
-                    <FaLocationArrow className="ms-3" color="#CBACF9" />
-                  </div>
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt="icon" className="p-2" />
+                    </div>
+                  ))}
                 </div>
-              </PinContainer>
-            </a>
+
+                <div className="flex justify-center items-center">
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
           </div>
         ))}
       </div>
